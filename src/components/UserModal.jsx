0dbd436fc6dd4ed49885eb5921c67d0853a99300
Alejandro Navarro <alejandropvp13@gmail.com>
2024-05-20 12:41:36 +0200

    Refactor UserModal component to improve UI and accessibility

--- src/components/UserModal.jsx
+++ src/components/UserModal.jsx
@@ -5,7 +5,7 @@ export const UserModal = ({ setChatScreen }) => {
   return (
     <div
       id="usersModal"
-      className="absolute left-1/2 px-2 transform -translate-x-1/2 min-w-96 border-2 border-gray-700 border-opacity-20 rounded-2xl flex flex-col items-center bg-gray-600 bg-opacity-25 justify-center"
+      className="absolute left-1/2 px-2 transform -translate-x-1/2 min-w-96 border-2 border-gray-700 border-opacity-20 rounded-2xl flex flex-col items-center bg-gray-600 bg-opacity-40 justify-center"
     >
       <div className="mt-2 text-white py-2 w-full px-6 flex items-center justify-between border-b-2 border-white">
         <h1 className="text-2xl font-medium ">Entorno CRA</h1>
